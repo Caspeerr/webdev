44ab9127e48d981e38a60e1fd2f411279602e573
Casperr <Xrestha9600@gmail.com>
2025-11-26 19:27:22 +0545

    id generator edit

--- assignment2/script.js
+++ assignment2/script.js
@@ -1,20 +1,30 @@
-const form = document.getElementById("userForm");
-const formContainer = document.getElementById("formContainer");
+const form = document.getElementById("studentForm");
+const formWrapper = document.getElementById("formWrapper");
 const idCard = document.getElementById("idCard");
 
-form.addEventListener("submit", function (e) {
+form.addEventListener("submit", function(e) {
   e.preventDefault();
 
   const name = document.getElementById("nameInput").value;
-  const idNum = document.getElementById("idNumberInput").value;
+  const studentID = document.getElementById("idInput").value;
   const faculty = document.getElementById("facultyInput").value;
-  const concentration = document.getElementById("concentrationInput").value;
+  const photoInput = document.getElementById("photoInput");
 
   document.getElementById("showName").textContent = name;
-  document.getElementById("showIdNumber").textContent = idNum;
+  document.getElementById("showID").textContent = studentID;
   document.getElementById("showFaculty").textContent = faculty;
-  document.getElementById("showConcentration").textContent = concentration;
 
-  formContainer.style.display = "none";
+  
+  if(photoInput.files && photoInput.files[0]){
+    const reader = new FileReader();
+    reader.onload = function(e){
+      document.getElementById("showPhoto").src = e.target.result;
+    }
+    reader.readAsDataURL(photoInput.files[0]);
+  } else {
+    document.getElementById("showPhoto").src = "";
+  }
+
+  formWrapper.style.display = "none";
   idCard.style.display = "flex";
 });
